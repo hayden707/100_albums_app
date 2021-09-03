bdbdc80581d3381f50dd201015e91e54e8eb2935
Hayden Anderson <anderson.hayden@gmail.com>
2021-09-03 12:09:31 -0400

    bug fix

--- server.js
+++ server.js
@@ -12,22 +12,22 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+
+app.use('/api', routes)
+app.use('/api/albums', routes)
+
+app.get('/middleware', (req, res, next) => {})
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/client/build/index.html`))
   })
 }
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(logger('dev'))
-app.use('/', routes)
-app.use('/api', routes)
-app.use('/api/albums', routes)
-
-app.get('/middleware', (req, res, next) => {})
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
