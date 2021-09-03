20dcc0918e2759b10b81842a4af11d52cf70d8cb
Hayden Anderson <anderson.hayden@gmail.com>
2021-09-03 11:13:27 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
