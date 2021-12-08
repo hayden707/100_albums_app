f72a269525c89cea9115a0ffcea89c3c58d86132
Hayden Anderson <anderson.hayden@gmail.com>
2021-12-08 14:16:02 -0500

    fixed mongodb link

--- db/index.js
+++ db/index.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let dbUrl =
+let MONGODB_URI =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
     : 'mongodb://127.0.0.1:27017/fridayAlbumsDatabase'
 
 mongoose
-  .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
