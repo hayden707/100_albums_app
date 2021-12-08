9b5dd6ca78a67d2978ef061c1e5144778bab6096
Hayden Anderson <anderson.hayden@gmail.com>
2021-12-08 16:46:07 -0500

    added console.log

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -10,6 +10,7 @@ export default function Home() {
     const res = await axios.get(`${BASE_URL}/albums`)
     setAlbums(res.data.albums)
     console.log(res.data.albums)
+    console.log('get albums function')
   }
 
   useEffect(() => {
