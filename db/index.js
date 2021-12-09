cf34158732521598c287dc98f2d7e0a7ca2bc4a3
Hayden Anderson <anderson.hayden@gmail.com>
2021-12-09 10:24:34 -0500

    test server.js

--- db/index.js
+++ db/index.js
@@ -1,24 +1,22 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let dbUrl =
-  process.env.NODE_ENV === 'production'
-    ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/fridayAlbumsDatabase'
+let MONGODB_URI
+
+if (process.env.NODE_ENV === 'production' || process.env.USE_ATLAS === 'true') {
+  MONGODB_URI = process.env.MONGODB_URI
+} else {
+  MONGODB_URI = 'mongodb://127.0.0.1:27017/fridayAlbumsDatabase'
+}
 
 mongoose
-  .connect(dbUrl, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: true
-  })
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-
 mongoose.set('debug', true)
 const db = mongoose.connection
 
