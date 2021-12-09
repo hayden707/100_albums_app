de09b807dd91c4611dcd1df04c7d1a575ebf12d9
Hayden Anderson <anderson.hayden@gmail.com>
2021-12-09 12:06:14 -0500

    deleted old heroku build

--- db/index.js
+++ db/index.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let MONGODB_URI
-
-if (process.env.NODE_ENV === 'production' || process.env.USE_ATLAS === 'true') {
-  MONGODB_URI = process.env.MONGODB_URI
-} else {
-  MONGODB_URI = 'mongodb://127.0.0.1:27017/fridayAlbumsDatabase'
-}
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/fridayAlbumsDatabase'
 
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
+
 mongoose.set('debug', true)
 const db = mongoose.connection
 
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
+
 app.use('/api', routes)
 
 app.get('/middleware', (req, res, next) => {})
