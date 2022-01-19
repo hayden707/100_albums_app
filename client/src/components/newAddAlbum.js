3636ab79af5711b729791ff2590d9e657b4f00bd
Hayden Anderson <anderson.hayden@gmail.com>
2022-01-19 17:22:42 -0500

    cors update2

--- client/src/components/newAddAlbum.js
+++ client/src/components/newAddAlbum.js
@@ -47,7 +47,7 @@ export default function NewAddAlbum() {
   }
 
   const scrape = (site) => {
-    axios(`https://thingproxy.freeboard.io/fetch/` + site).then((res) => {
+    axios(`http://gobetween.oklabs.org/` + site).then((res) => {
       const html = res.data
       const $ = cheerio.load(html)
       const genreList = []
