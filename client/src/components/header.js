cec573d82c62f928437406d855815c9c978a10c7
Hayden Anderson <anderson.hayden@gmail.com>
2022-03-23 17:24:20 -0400

    updated line height nav bar

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './header.css'
 
 export default function Header() {
   return (
-    <header>
+    <header className="nav">
       <nav>
         <NavLink className="navlink" to="/">
           new albums
